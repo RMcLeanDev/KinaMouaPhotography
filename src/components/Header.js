5d2451022203272b60bfe55cdb4916a3d58b3b72
RMcLeanDev <rmcleandev@gmail.com>
2021-10-19 20:31:54 -0700

    fix issue where full mobile header wasn't displaying correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,11 +46,13 @@ function Header(){
             <span></span>
           </div>
           <div className={headerDisplay.menu}>
-            <Link exact to='/About' onClick={() => setShowHide(false)}><h3>About</h3></Link>
-            <Link to='/Contact' onClick={() => setShowHide(false)}><h3>Book Me</h3></Link>
-            <Link to='/Prices' onClick={() => setShowHide(false)}><h3>Prices</h3></Link>
-            <Link to='/Testimonials' onClick={() => setShowHide(false)}><h3>Testimonials</h3></Link>
-            <a target="_blank" href="https://www.instagram.com/kinamoua/?hl=en"><img src={instagram}/></a>
+            <div className="insideMobileHeader">
+              <Link exact to='/About' onClick={() => setShowHide(false)}><h3>About</h3></Link>
+              <Link to='/Contact' onClick={() => setShowHide(false)}><h3>Book Me</h3></Link>
+              <Link to='/Prices' onClick={() => setShowHide(false)}><h3>Prices</h3></Link>
+              <Link to='/Testimonials' onClick={() => setShowHide(false)}><h3>Testimonials</h3></Link>
+              <a target="_blank" href="https://www.instagram.com/kinamoua/?hl=en"><img src={instagram}/></a>
+            </div>
         </div>
       </div>
     </div>
