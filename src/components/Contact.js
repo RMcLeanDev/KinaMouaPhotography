fd8363597d62ada49f2492193c3afffcb513a6b5
RMcLeanDev <rmcleandev@gmail.com>
2021-10-21 20:30:56 -0700

    add emailjs to project and create form for users to send email to owner, add styling to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,38 @@
-import react from 'react';
+import react, {useRef} from 'react';
+import "../scss/ContactMe.scss";
+import emailjs from 'emailjs-com';
 
 function Contact(){
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        console.log("kalsdhkfj")
+    
+        emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, 'template_gpiy5r9', form.current, process.env.REACT_APP_EMAILJS)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+
+      };
+
     return(
-        <div>
-            <h1>Contact Page</h1>
+        <div className="contactMe">
+            <div>
+                <h1><em>Contact</em></h1>
+                <hr/>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="from_name" required placeholder="Name:"/>
+                    <input type="email" name="reply_to" required placeholder="Email:"/>
+                    <textarea name="message" placeholder="Message:"/>
+                    <button type="submit" value="Send" required> Send </button>
+                </form>
+                <hr/>
+            </div>
         </div>
     )
 }
