cf73dc8c97b373c4c1f2c5626b1cf1bb876622f7
RMcLeanDev <rmcleandev@gmail.com>
2024-01-27 11:45:45 -0800

    fix email form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import react, {useRef, useState} from 'react';
 import "../scss/ContactMe.scss";
+import emailjs from '@emailjs/browser'
 
 function Contact(){
 
@@ -10,12 +11,12 @@ function Contact(){
     const sendEmail = (e) => {
         e.preventDefault();
     
-        /*emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, 'template_g2z8q1p', form.current, process.env.REACT_APP_EMAILJS)
+        emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, 'template_nl4go8d', form.current, process.env.REACT_APP_EMAILJS)
           .then((result) => {
             setAlert(<h1>Thank you for your inquiry! I will reach out to you ASAP!</h1>)
         }, (error) => {
             setAlert(<p>{error.text}</p>)
-        });*/
+        });
 
       };
 
