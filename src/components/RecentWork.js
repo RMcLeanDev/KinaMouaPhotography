8bb49fbc74cc54a8a33ba890c09fcfc9d6134d57
RMcLeanDev <rmcleandev@gmail.com>
2024-01-27 12:30:06 -0800

    add more images

--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -4,6 +4,10 @@ import image1 from '../assets/image0.jpeg';
 import image2 from '../assets/image1.jpeg';
 import image3 from '../assets/image2.jpeg';
 import image4 from '../assets/image3.jpeg';
+import image5 from '../assets/image0.6dbc1fe9.jpeg'
+import image6 from '../assets/image2.42dee657.jpeg'
+import image7 from '../assets/image4.jpeg'
+import image8 from '../assets/image06.jpeg'
 import '../scss/RecentWork.scss';
 import 'aos/dist/aos.css';
 
@@ -17,6 +21,10 @@ function RecentWork(){
             <img src={image4} data-aos="fade-up" alt=""/>     
             <img src={image2} data-aos="fade-up" alt=""/>
             <img src={image3} data-aos="fade-up" alt=""/> 
+            <img src={image5} data-aos="fade-up" alt=""/> 
+            <img src={image6} data-aos="fade-up" alt=""/> 
+            <img src={image7} data-aos="fade-up" alt=""/> 
+            <img src={image8} data-aos="fade-up" alt=""/> 
         </div>
     )
 }
