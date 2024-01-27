a1728e83d00c0308433fdfc7b079751e08023792
RMcLeanDev <rmcleandev@gmail.com>
2024-01-27 12:53:20 -0800

    adjusting mobile recent work as client requested

--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -13,19 +13,26 @@ import 'aos/dist/aos.css';
 
 function RecentWork(){
 
-    console.log(useImageSize(image1)[0]);
+    let size = useImageSize(image1)[0];
+    console.log(size)
 
     return(
-        <div className="recentWork">
-            <img src={image1} data-aos="fade-up" alt=""/> 
-            <img src={image4} data-aos="fade-up" alt=""/>     
-            <img src={image2} data-aos="fade-up" alt=""/>
-            <img src={image3} data-aos="fade-up" alt=""/> 
-            <img src={image5} data-aos="fade-up" alt=""/> 
-            <img src={image6} data-aos="fade-up" alt=""/> 
-            <img src={image7} data-aos="fade-up" alt=""/> 
-            <img src={image8} data-aos="fade-up" alt=""/> 
-        </div>
+        <div>
+            {
+            size ? 
+            <div className="recentWork">
+                <img style={{maxHeight: "80vh"}} src={image1} data-aos="fade-up" alt=""/> 
+                <img style={{maxHeight: "80vh"}} src={image4} data-aos="fade-up" alt=""/>     
+                <img style={{maxHeight: "80vh"}} src={image2} data-aos="fade-up" alt=""/>
+                <img style={{maxHeight: "80vh"}} src={image3} data-aos="fade-up" alt=""/> 
+                <img style={{maxHeight: "80vh"}} src={image5} data-aos="fade-up" alt=""/> 
+                <img style={{maxHeight: "80vh"}} src={image6} data-aos="fade-up" alt=""/> 
+                <img style={{maxHeight: "80vh"}} src={image7} data-aos="fade-up" alt=""/> 
+                <img style={{maxHeight: "80vh"}} src={image8} data-aos="fade-up" alt=""/> 
+            </div>
+            : "loading"
+        }
+        </div>  
     )
 }
 
