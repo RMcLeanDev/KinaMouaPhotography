65980a320f12aa0588f2dad4f9f6924d58fa1265
RMcLeanDev <rmcleandev@gmail.com>
2024-05-03 19:45:37 -0700

    update recent work and fix styling

--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -13,13 +13,17 @@ import image9 from '../assets/image12.jpeg'
 import image10 from '../assets/image22.jpeg'
 import image11 from '../assets/image02.jpeg'
 import image12 from '../assets/image32.jpeg'
+import image13 from '../assets/image1.1.jpeg';
+import image14 from '../assets/image2.1.jpeg';
+import image15 from '../assets/image3.1.jpeg';
+import image16 from '../assets/image4.1.jpeg';
 import ZoomImage from './ZoomImage';
 import '../scss/RecentWork.scss';
 import 'aos/dist/aos.css';
 
 function RecentWork(){
 
-    const arry = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+    const arry = [image15, image14, image16, image13, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
     const [zoom, setZoom] = useState({show: false, imageInfo: null});
 
     return(
